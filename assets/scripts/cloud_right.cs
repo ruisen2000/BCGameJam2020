ca10852dd8037171ec314a5f828dc4be4577933b
maheeppartap <44275375+maheeppartap@users.noreply.github.com>
2020-02-15 12:41:59 -0800

    new assets and menu

---
+++ assets/scripts/cloud_right.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class cloud_right : AnimatedSprite
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public int cloud_velocity = 3;
+	public override void _Ready()
+	{
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+	  Vector2 mm = GetPosition();
+		mm.x += cloud_velocity/2;
+		
+		SetPosition(mm);
+		
+  }
+}
