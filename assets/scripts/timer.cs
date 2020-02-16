e07ad60a7f3f79944b3ec0a5c7df5c84ff1072f2
Isvan <iavorsivanov@gmail.com>
2020-02-15 16:14:21 -0800

    More Menu updates

--- assets/scripts/timer.cs
+++ assets/scripts/timer.cs
@@ -1,24 +1,30 @@
 using Godot;
 using System;
 
-public class new_game : TextureButton
+public class timer : Node2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
+	Label label;
+	Timer timer_;
+
 	public override void _Ready()
 	{
-		
+		timer_ = (Timer)GetNode("myTimer");
+		label = (Label)GetNode("display");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
  {
-		if (Pressed)
+		label.Text = ((int)timer_.TimeLeft).ToString();
+
+		if(label.Text == "0")
 		{
-			GetTree().ChangeScene("res://assets/scenes/counters.tscn");
+			GetTree().ChangeScene("res://assets/scenes/Roping.tscn");
 		}
-	}
+ }
 }
