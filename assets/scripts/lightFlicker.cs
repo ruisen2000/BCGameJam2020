0efa08174e926e319774bcc90ad74c5b77bba80a
Braden Emerson <inthedeepstuff@gmail.com>
2020-02-16 12:08:19 -0800

    Added some torches and a flicker effect to them.

--- assets/scripts/lightFlicker.cs
+++ assets/scripts/lightFlicker.cs
@@ -4,18 +4,29 @@ using System;
 public class lightFlicker : Light2D
 {
 	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	[Export]
+	float period = 1;
+	[Export]
+	float maxSize = 3;
+	[Export]
+	float minSize = 2.5f;
+
+	private float sizeDiff;
+	private float time = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		sizeDiff = maxSize - minSize;
+		TextureScale = (sizeDiff / 2) * (float)Math.Sin(Math.PI / (2 * period) * time) + ((minSize + maxSize) / 2);
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		// flicker the flame
+		time += delta;
+		TextureScale = (sizeDiff/2) * (float)Math.Sin(Math.PI/(2*period) * time) + ((minSize + maxSize)/2);
+
+	}
 }
