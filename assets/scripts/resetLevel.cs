465f84d4100adc7d0cbc8ea82c156ac961b69671
Isvan <iavorsivanov@gmail.com>
2020-02-16 16:29:58 -0800

    More las min changes, this is getting forced agian

--- assets/scripts/resetLevel.cs
+++ assets/scripts/resetLevel.cs
@@ -1,30 +1,29 @@
 using Godot;
 using System;
 
-
-public class reset : TextureButton
+public class resetLevel : Node2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
-
 	public override void _Ready()
 	{
-
+		
 	}
+
 
 	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		if (Input.IsActionPressed("reset"))
+				if (Input.IsActionPressed("reset"))
 		{
 			GetTree().ReloadCurrentScene();
-		}else if (Input.IsActionPressed("exit"))
+		}else if (Input.IsActionPressed("quit"))
 		{
 			GetTree().Quit();
 		}
-
+		
 	}
 }
